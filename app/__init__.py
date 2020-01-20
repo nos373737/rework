4ab2729288819bd7d348ae8ac249260e8456a843
nos373737 <nosandriy@gmail.com>
2020-01-20 23:10:21 +0200

    Cable search ui

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
+from app.index import MyIndexView
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
@@ -9,6 +10,6 @@ logging.getLogger().setLevel(logging.DEBUG)
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLA(app)
-appbuilder = AppBuilder(app, db.session, base_template="mybase.html")
+appbuilder = AppBuilder(app, db.session, base_template="mybase.html", indexview=MyIndexView)
 
 from . import models, views  # noqa
